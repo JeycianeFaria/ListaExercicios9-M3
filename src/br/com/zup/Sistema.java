6dfbc49a77e31b4f07f522a9c7aff31a58e37429
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-10-07 16:14:04 -0300

    Adicionar método para exibir o menu para o usuario dos tipos de pessoas

--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -1,5 +1,6 @@
 package br.com.zup;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Sistema {
@@ -14,6 +15,15 @@ public class Sistema {
         System.out.println("Digite 1 - para cadastrar um consumidor.");
         System.out.println("Digite 2 - para cadastrar uma fatura.");
         System.out.println("Digite 3 - para sair.");
+    }
+
+    public  static void menuTipoPessoa(){
+        List<TipoPessoa> tipoPessoas = ServicoConsumidor.exibirTiposPessoas();
+        System.out.println("======Tipos de Pessoas======");
+
+        for (int contador = 0; contador < tipoPessoas.size();contador++){
+            System.out.println("Digite " + contador + " para: " + tipoPessoas.get(contador));
+        }
     }
 
 }
