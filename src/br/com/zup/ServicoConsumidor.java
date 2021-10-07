96085c402200208eca30677a054005adcc3c3936
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-10-07 16:04:33 -0300

    Adicionar método para exibir os tipos de pessoas

--- src/br/com/zup/ServicoConsumidor.java
+++ src/br/com/zup/ServicoConsumidor.java
@@ -4,5 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ServicoConsumidor {
+
     private static List<Consumidor> consumidores = new ArrayList<>();
+
+    public  static List<TipoPessoa> exibirTiposPessoas(){
+        List<TipoPessoa> tiposPessoas = new ArrayList<>();
+
+        for (TipoPessoa referencia: TipoPessoa.values()){
+            tiposPessoas.add(referencia);
+        }
+
+        return tiposPessoas;
+
+    }
+
 }
--- src/br/com/zup/ServicoFatura.java
+++ src/br/com/zup/ServicoFatura.java
@@ -1,4 +1,10 @@
 package br.com.zup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServicoFatura {
+
+    private static List<Fatura> faturas = new ArrayList<>();
+
 }
